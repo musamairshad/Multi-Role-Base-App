46973c7d3945237f7f5038e0d424d0ac3bd9e6ce
Muhammad Usama <imusamairshad@gmail.com>
2023-03-28 00:53:47 +0500

    modified home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,17 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import './login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   static const routeName = "/home-screen";
+
+  const HomeScreen({super.key});
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _email = '', _age = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  _loadData() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    _email = sp.getString('Email') ?? '';
+    _age = sp.getString('Age') ?? '';
+    setState(() {
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Email'),
+                Text(_email.toString(),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Age'),
+                Text(_age.toString(),
+                ),
+              ],
+            ),
+            const SizedBox(height: 40),
+            InkWell(
+              onTap: () async {
+                SharedPreferences sp = await SharedPreferences.getInstance();
+                sp.clear(); // all the data will be clear.
+
+                // ignore: use_build_context_synchronously
+                Navigator.pushNamed(context, LoginScreen.routeName);
+              },
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: const Center(
+                  child: Text(
+                    "Logout",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
